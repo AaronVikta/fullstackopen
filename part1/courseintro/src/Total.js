d3ec0bfad60e2d1d4a0cfcdbda70d2a52debdf0f
Aaron Victor <aeinsteinvikta@gmail.com>
2022-05-28 23:46:31 +0100

    Exercise 1.1
    Submission for exercise 1.1: Course Information

---
+++ part1/courseintro/src/Total.js
@@ -0,0 +1,10 @@
+const Total =(props)=>{
+    return(
+        <>
+            <p>
+                Total number of exercise is {props.exercises1 + props.exercises2 + props.exercises3}
+            </p>
+        </>
+    )
+}
+export default Total
