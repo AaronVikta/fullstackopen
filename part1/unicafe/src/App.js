a42f6adffe4646266008e514c031f9bf8116d6e5
Aaron Victor <aeinsteinvikta@gmail.com>
2022-06-05 22:52:23 +0100

    Exercise 1.8
     unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import Statistics from './Statistics'
 
 function App() {
 
@@ -17,24 +17,7 @@ function App() {
    setNeutral(neutral+1)
  }
  const all = (good +bad+neutral)
- const average =(good, neutral, bad)=>{
-   let badprod = bad*-1
-   let goodprod = good*1
-   let neutralprod = neutral*0
-
-   let avg = goodprod+badprod+neutralprod
-
-   return avg
- }
-
- const percentage = (good, neutral, bad)=>{
-   let sum = good+neutral+bad
-  let perc = (good/sum)*100
-    if (sum === 0){
-      return 'Infinity'
-    }
-  return `${perc} %`
- }
+ 
   return (
     <div>
       <h1>give feedback</h1>
@@ -43,12 +26,14 @@ function App() {
       <button onClick={handleBad}>bad</button>
       
       <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad} </p>
-      <p>all {all} </p>
-      <p>Average {average(good, neutral, bad)}</p>
-      Positive {percentage(good,neutral,bad)}
+      <Statistics
+      good={good}
+      bad ={bad}
+      neutral ={neutral}
+      all= {all}
+      />
+   
+     
     </div>
   );
 }
---
+++ part1/unicafe/src/Statistics.js
@@ -0,0 +1,34 @@
+const Statistics =({good, neutral, bad,all})=>{
+
+    const average =(good, neutral, bad)=>{
+        let badprod = bad*-1
+        let goodprod = good*1
+        let neutralprod = neutral*0
+     
+        let avg = goodprod+badprod+neutralprod
+     
+        return avg
+      }
+     
+      const percentage = (good, neutral, bad)=>{
+        let sum = good+neutral+bad
+       let perc = (good/sum)*100
+         if (sum === 0){
+           return 'Infinity'
+         }
+       return `${perc} %`
+      }
+      
+    return(
+        <div>
+            <p>good {good}</p>
+            <p>neutral {neutral} </p>
+            <p>bad {bad} </p>
+
+            <p>all {all} </p>
+            <p>Average {average(good, neutral, bad)}</p>
+            Positive {percentage(good, neutral, bad)}
+        </div>
+    )
+}
+export default Statistics
